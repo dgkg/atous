80b169d55e739bf9f70ab8a29b820170e1d4d514
Henri LEPIC <henrilepic@gmail.com>
2023-01-23 12:46:15 +0100

    init atous.

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/dgkg/atous
+
+go 1.19
+
+require github.com/davecgh/go-spew v1.1.1
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	spew.Dump("Hello World")
+}
