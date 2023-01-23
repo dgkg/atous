3a7881916d3e659225fd24717a8591f9342274ca
Henri LEPIC <henrilepic@gmail.com>
2023-01-23 15:26:29 +0100

    add map tp.

--- main.go
+++ main.go
@@ -19,4 +19,34 @@ func main() {
 	config.Exec()
 	spew.Dump(config.Login)
 	spew.Dump(config.Pass)
+
+	m := make(map[string]string)
+	m["key"] = "value"
+	spew.Dump(m)
+
+	StatusCodes := map[int]string{
+		200: "OK",
+		201: "Created",
+		202: "Accepted",
+	}
+	fmt.Println(StatusCodes[200])
+	MapTP()
+}
+
+func MapTP() {
+	//	Créez et initialisez une map m avec en clée des strings et en valeur des ints
+	m := make(map[string]int)
+	// Créez une variable de type rune nommée letter et initialiser sa valeur à 'a'
+	letter := 'a'
+	// Créez une boucle for avec i comme itérateur sur 26 éléments
+	for i := 0; i < 26; i++ {
+		// Insérer dans m la valeur de letter après l’avoir casté en string
+		// comme clée et comme valeur insérer l’incrément i de la boucle for
+		m[string(letter)] = i
+		// Auto-incrémentez la valeur de letter
+		letter++
+	}
+	// Après la boucle for afficher la valeur de la clé "w"
+	// dans la map m via la fonction fmt.Println( )
+	fmt.Println(m["w"])
 }
