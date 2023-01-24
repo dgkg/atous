7785b0ec8f3537f6053d7c94e1ff171fbf0d9fdd
Henri LEPIC <henrilepic@gmail.com>
2023-01-24 09:49:21 +0100

    create user.

--- main.go
+++ main.go
@@ -1,97 +1,30 @@
 package main
 
 import (
+	"atous/model"
 	"fmt"
+	"net/http"
 
-	"github.com/davecgh/go-spew/spew"
-
-	"atous/config"
+	"github.com/gin-gonic/gin"
 )
 
 func init() {
-	fmt.Println("Init")
+	fmt.Println("Init project")
 }
 
 func main() {
-	fmt.Println("Hello World")
-	spew.Dump("Hello World")
+	r := gin.Default()
+	r.POST("/users", createUser)
+	r.Run()
+}
 
-	config.Exec()
-	spew.Dump(config.Login)
-	spew.Dump(config.Pass)
-
-	m := make(map[string]string)
-	m["key"] = "value"
-	spew.Dump(m)
-
-	StatusCodes := map[int]string{
-		200: "OK",
-		201: "Created",
-		202: "Accepted",
+func createUser(c *gin.Context) {
+	var user model.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	}
-	fmt.Println(StatusCodes[200])
-	MapTP()
 
-	// tbl := make([]uint, 0, 10)
-	// tbl = []uint{1, 2, 3, 10, 20, 30}
-	// lenTbl := len(tbl) * 200
-	// for i := 0; i < lenTbl; i++ {
-	// 	//fmt.Println(&tbl[i])
-	// 	tbl = append(tbl, uint(i))
-	// 	fmt.Println("len:", len(tbl), "cap:", cap(tbl))
-	// }
-
-	u := User{
-		id:        1,
-		FirstName: "John",
-		LastName:  "Doe",
-		Age:       42,
-		RoleType:  Employee,
-	}
-	fmt.Println(u)
-}
-
-type Role int
-
-const (
-	Admin Role = iota
-	Manager
-	Employee
-	MaxRole
-)
-
-var roleType = [MaxRole]string{
-	Admin:    "role admin",
-	Manager:  "role manager",
-	Employee: "role employee",
-}
-
-func (r Role) String() string {
-	return roleType[r]
-}
-
-type User struct {
-	id        int
-	FirstName string
-	LastName  string
-	Age       int
-	RoleType  Role
-}
-
-func MapTP() {
-	//	Créez et initialisez une map m avec en clée des strings et en valeur des ints
-	m := make(map[string]int)
-	// Créez une variable de type rune nommée letter et initialiser sa valeur à 'a'
-	letter := 'a'
-	// Créez une boucle for avec i comme itérateur sur 26 éléments
-	for i := 0; i < 26; i++ {
-		// Insérer dans m la valeur de letter après l’avoir casté en string
-		// comme clée et comme valeur insérer l’incrément i de la boucle for
-		m[string(letter)] = i
-		// Auto-incrémentez la valeur de letter
-		letter++
-	}
-	// Après la boucle for afficher la valeur de la clé "w"
-	// dans la map m via la fonction fmt.Println( )
-	fmt.Println(m["w"])
+	user = *model.NewUser(user.FirstName, user.LastName, user.Age)
+	c.JSON(http.StatusOK, gin.H{"user": user})
 }
---
+++ model/user.go
@@ -0,0 +1,46 @@
+package model
+
+import (
+	"github.com/google/uuid"
+)
+
+type Role int
+
+const (
+	Admin Role = iota + 1
+	RestorantManager
+	Customer
+	Driver
+	Undefined
+	MaxRole
+)
+
+var roleType = [MaxRole]string{
+	Admin:            "admin",
+	RestorantManager: "restorant manager",
+	Customer:         "customer",
+	Driver:           "driver",
+	Undefined:        "undefined",
+}
+
+func (r Role) String() string {
+	return roleType[r]
+}
+
+type User struct {
+	ID        string `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Age       int    `json:"age"`
+	RoleType  Role   `json:"role_type"`
+}
+
+func NewUser(firstName, lastName string, age int) *User {
+	return &User{
+		ID:        uuid.NewString(),
+		FirstName: firstName,
+		LastName:  lastName,
+		Age:       age,
+		RoleType:  Customer,
+	}
+}
