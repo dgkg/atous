c3bf7a7b676b0f4f7e11426ef025b38c82935136
Henri LEPIC <henrilepic@gmail.com>
2023-01-27 08:42:08 +0100

    model: add doc.

--- model/model.go
+++ model/model.go
@@ -2,9 +2,12 @@ package model
 
 import "time"
 
+// DBData is the base struct for all objects stored in the database.
 type DBData struct {
+	// ID is the unique identifier of the object.
 	ID string `json:"id"`
-	// DB dates.
+	// CreateAt is the time when the object was created.
 	CreateAt time.Time `json:"create_at"`
+	// UpdateAt is the time when the object was last updated.
 	UpdateAt time.Time `json:"update_at"`
 }
